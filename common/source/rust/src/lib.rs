f299f5b2ad9323b1e1dcb3292e3cec84cd034345
dreistein <dev@0xd3.xyz>
2021-02-16 09:45:03 +0100

    inline assembler example

--- common/source/rust/src/lib.rs
+++ common/source/rust/src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![feature(alloc_error_handler)]
+#![feature(asm)]
 
 use core::panic::PanicInfo;
 
@@ -19,12 +20,39 @@ pub unsafe extern "C" fn add_one(val: u64) -> u64 {
     let mut v = Vec::new();
 
     println!("Hello from Rust!");
-
+    
+    // cache_size();
+    // panic!("Panics work too!");
+    // dbg!(v[2]); // this will panic
+    
     v.push(val);
     v.push(1);
-    //panic!("Panics work too!");
     v.into_iter().sum::<u64>()
 }
+
+
+// see https://doc.rust-lang.org/unstable-book/library-features/asm.html
+#[no_mangle]
+pub unsafe extern "C" fn cache_size() -> u32 {
+    let ebx: u32;
+    let ecx: u32;
+
+    asm!(
+        "cpuid",
+        // EAX 4 selects the "Deterministic Cache Parameters" CPUID leaf
+        inout("eax") 4 => _,
+        // ECX 0 selects the L0 cache information.
+        inout("ecx") 0 => ecx,
+        lateout("ebx") ebx,
+        lateout("edx") _,
+    );
+
+    let size = ((ebx >> 22) + 1) * (((ebx >> 12) & 0x3ff) + 1) * ((ebx & 0xfff) + 1) * (ecx + 1);
+    println!("L1 Cache: {}", size);
+    
+    size
+}
+
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
